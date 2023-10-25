0d2d589681c24706e9bd1ff8c1acb6b42d713dcd
Dewa made <108676698+DewaMadeWira@users.noreply.github.com>
2023-10-25 08:17:21 +0700

    move up folder

--- new-window/src/Notes.tsx
+++ new-window/src/Notes.tsx
@@ -2,17 +2,20 @@ import { useState } from 'react';
 
 // import './App.css';
 import { useToast } from './radix/components/ui/use-toast';
+import { useMediaQuery } from 'react-responsive';
 import Button from './components/button';
+
 type ParamsType = {
     params: string;
 };
 function Notes() {
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
     const [count, setCount] = useState(0);
     const { toast } = useToast();
     return (
         <div className='flex flex-col gap-10 items-center font-epilogue h-screen '>
             <h1 className='text-5xl font-bold mt-10 sm:text-6xl xl:text-7xl'>
-                This is Notes
+                {isTabletOrMobile && <p>You are a tablet or mobile phone</p>}
             </h1>
             <h2 className='text-2xl sm:text-3xl md:text-4xl'>
                 For passing your{' '}
